dae031ed4f04adf9b1a7e456f3abb09b3d8894c6
wang.jia.le <wjmx1214@sina.com>
2021-03-05 18:33:12 +0800

    Update BaseDAO2.java
    update to 1.0.1

--- src/main/java/com/boot/dao/impl/BaseDAO2.java
+++ src/main/java/com/boot/dao/impl/BaseDAO2.java
@@ -8,7 +8,7 @@ import com.boot.dao.api.IBaseDAO2;
 /**
  * 数据源datasource2对应的DAO
  * @author 2020-12-01 create wang.jia.le
- * @version 1.0.0
+ * @version 1.0.1
  */
 @Repository
 @ConditionalOnProperty(prefix="spring.datasource.datasource2", name="username") //配置了才注册
