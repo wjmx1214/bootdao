8d0438455e424d665e06f218de97d9c6f5bd55f0
wang.jia.le <wjmx1214@sina.com>
2021-03-05 18:24:10 +0800

    Update EntityPath.java
    update to 1.0.1

--- src/main/java/com/boot/dao/api/EntityPath.java
+++ src/main/java/com/boot/dao/api/EntityPath.java
@@ -11,14 +11,14 @@ import java.lang.annotation.Target;
 	用于entity、dto、vo等无差别无感知调用, 优先于统一路径配置<br>
 	注意：此注解定义为实体类全路径(含类名)，统一配置只有包路径<br>
 	例：<pre>
-	@EntityPath("com.xxx.xxx.entity.Student")
+	+@EntityPath("com.xxx.xxx.entity.Student")
 	public class StuVo {
 		private Long id;
 		private String name;
 	}
 	</pre>
 	@author 2020-12-01 create wang.jia.le
-	@version 1.0.0
+	@version 1.0.1
 **/
 @Retention(RetentionPolicy.RUNTIME) 			// 注解会在class字节码文件中存在，在运行时可以通过反射获取到
 @Target({ElementType.TYPE}) 					// 定义注解的作用目标(类，常量，字段，方法等)
@@ -27,8 +27,8 @@ public @interface EntityPath {
 
 	/**
 	 * 实体类全路径(com.xxx.xxx.entity.Student)
-	 * @return
+	 * @return String
 	 */
 	 String value() default "";
 
-}  
+}
