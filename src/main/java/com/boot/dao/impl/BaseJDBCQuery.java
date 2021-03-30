68ea79d49987b84ff9113e9c191c76148c7fad4e
wang.jia.le <wjmx1214@sina.com>
2021-03-30 16:24:54 +0800

    Update BaseJDBCQuery.java

--- src/main/java/com/boot/dao/impl/BaseJDBCQuery.java
+++ src/main/java/com/boot/dao/impl/BaseJDBCQuery.java
@@ -3,15 +3,19 @@ package com.boot.dao.impl;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.boot.dao.config.BaseDAOConfig;
+import com.boot.dao.mapping.BaseColumnMapping;
+import com.boot.dao.mapping.BaseTableMapping;
 import com.boot.dao.util.BaseDAOLog;
 import com.boot.dao.util.BaseDAOUtil;
 
 /**
  * 查询封装类
  * @author 2020-12-01 create wang.jia.le
- * @version 1.0.1
+ * @version 1.0.3
  */
 class BaseJDBCQuery {
 
@@ -20,6 +24,8 @@ class BaseJDBCQuery {
 	ResultSet rs;
 
 	int columnCount = 0;
+	
+	List<BaseColumnMapping> resultColumns;
 	
 	/*
 	 * 执行
@@ -33,7 +39,29 @@ class BaseJDBCQuery {
 		this.ps = conn.prepareStatement(sql);
 		BaseDAOUtil.setParams(ps, params); 	//设置参数
 		this.rs = ps.executeQuery(); 		//获取结果集
-		columnCount = rs.getMetaData().getColumnCount();
+		this.columnCount = rs.getMetaData().getColumnCount();
+		return this.rs;
+	}
+	
+	/*
+	 * 执行
+	 * @param tm
+	 * @param conn
+	 * @param sql
+	 * @param params
+	 * @throws Exception
+	 */
+	ResultSet query(BaseTableMapping tm, Connection conn, String sql, Object... params) throws Exception {
+		BaseDAOLog.printSQLAndParam(BaseDAOConfig.showSQL, BaseDAOConfig.showParam, sql, params);
+		this.ps = conn.prepareStatement(sql);
+		BaseDAOUtil.setParams(ps, params); 	//设置参数
+		this.rs = ps.executeQuery(); 		//获取结果集
+		this.columnCount = rs.getMetaData().getColumnCount();
+		this.resultColumns = new ArrayList<>();
+		for(int i=0; i<this.columnCount; i++) {
+			BaseColumnMapping cm = tm.columnMappings.get(this.rs.getMetaData().getColumnLabel(i+1).toLowerCase());
+			this.resultColumns.add(cm);
+		}
 		return this.rs;
 	}
 	
@@ -44,6 +72,7 @@ class BaseJDBCQuery {
 		try {
 			if(rs != null) rs.close();
 			if(ps != null) ps.close();
+			if(resultColumns != null) resultColumns = null;
 		} catch (Exception e) {
 			BaseDAOLog.printException(e);
 		}
