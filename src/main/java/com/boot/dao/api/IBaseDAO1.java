1e5382f83e48e4e0988bc42546c3d39068a9e4d3
wang.jia.le <wjmx1214@sina.com>
2021-04-07 20:16:46 +0800

    Update IBaseDAO1.java

--- src/main/java/com/boot/dao/api/IBaseDAO1.java
+++ src/main/java/com/boot/dao/api/IBaseDAO1.java
@@ -1,8 +1,8 @@
 package com.boot.dao.api;
 
 /**
- * 数据源datasource1对应的DAO
+ * 数据源dataSource1对应的DAO
  * @author 2020-12-01 create wang.jia.le
- * @version 1.0.1
+ * @version 1.0.4
  */
 public interface IBaseDAO1 extends IBaseEntityDAO{}
