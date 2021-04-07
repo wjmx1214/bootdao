a79d8b19c42056203acbc443ec1fa65c957af867
wang.jia.le <wjmx1214@sina.com>
2021-04-07 20:20:55 +0800

    Update BaseSourceMoreConfig.java

--- src/main/java/com/boot/dao/config/BaseSourceMoreConfig.java
+++ src/main/java/com/boot/dao/config/BaseSourceMoreConfig.java
@@ -6,10 +6,9 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -22,10 +21,9 @@ import com.boot.dao.util.BaseDAOLog;
 /**
  * 多数据源默认配置类
  * @author 2020-12-01 create wang.jia.le
- * @version 1.0.1
+ * @version 1.0.4
  */
 @Configuration
-@AutoConfigureBefore(DataSourceAutoConfiguration.class)
 public class BaseSourceMoreConfig {
 
 	private static String druidType = "com.alibaba.druid.pool.DruidDataSource";
@@ -39,32 +37,7 @@ public class BaseSourceMoreConfig {
 			buildMethod = druidDataSourceBuilderClass.getDeclaredMethod("build");
 		} catch (Exception e) {}
 	}
-
-    @Bean
-    @Primary
-    @ConditionalOnProperty(prefix="spring.datasource.datasource0", name="username")
-    @ConfigurationProperties(prefix="spring.datasource.datasource0")
-    @Value("${spring.datasource.datasource0.type:#{null}}")
-    public DataSource dataSource(String type) {
-    	return createDataSource(type);
-    }
 	
-    @Bean
-    @ConditionalOnProperty(prefix="spring.datasource.datasource1", name="username")
-    @ConfigurationProperties(prefix="spring.datasource.datasource1")
-    @Value("${spring.datasource.datasource1.type:#{null}}")
-    public DataSource datasource1(String type) {
-    	return createDataSource(type);
-    }
-
-    @Bean
-    @ConditionalOnProperty(prefix="spring.datasource.datasource2", name="username")
-    @ConfigurationProperties(prefix="spring.datasource.datasource2")
-    @Value("${spring.datasource.datasource2.type:#{null}}")
-    public DataSource datasource2(String type) {
-    	return createDataSource(type);
-    }
-    
     private DataSource createDataSource(String type) {
     	DataSource dataSource = null;
     	if(type != null && druidType.equals(type)) {
@@ -80,27 +53,77 @@ public class BaseSourceMoreConfig {
     	}
     	return dataSource;
     }
+
+    @Bean
+    @Primary
+    @ConditionalOnMissingBean(name = "dataSource")
+    @ConditionalOnProperty(prefix="spring.datasource", name="username")
+    @ConfigurationProperties(prefix="spring.datasource")
+    @Value("${spring.datasource.type:#{null}}")
+    public DataSource dataSource(String type) {
+    	return createDataSource(type);
+    }
+	
+    @Bean
+    @ConditionalOnProperty(prefix="spring.datasource1", name="username")
+    @ConfigurationProperties(prefix="spring.datasource1")
+    @Value("${spring.datasource1.type:#{null}}")
+    public DataSource dataSource1(String type) {
+    	return createDataSource(type);
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix="spring.datasource2", name="username")
+    @ConfigurationProperties(prefix="spring.datasource2")
+    @Value("${spring.datasource2.type:#{null}}")
+    public DataSource dataSource2(String type) {
+    	return createDataSource(type);
+    }
     
-    //-------------------------------------------------------配置事务管理器-------------------------------------------------
+    //----------------------------------------------druid-----------------------------------------------------
+    
+    @Bean(name = "dataSource")
+    @Primary
+    @ConditionalOnMissingBean(name = "dataSource")
+    @ConditionalOnProperty(prefix="spring.datasource.druid", name="username")
+    @ConfigurationProperties(prefix="spring.datasource.druid")
+    public DataSource druidDataSource() {
+    	return createDataSource(druidType);
+    }
+	
+    @Bean(name = "dataSource1")
+    @ConditionalOnProperty(prefix="spring.datasource1.druid", name="username")
+    @ConfigurationProperties(prefix="spring.datasource1.druid")
+    public DataSource druidDataSource1() {
+    	return createDataSource(druidType);
+    }
+
+    @Bean(name = "dataSource2")
+    @ConditionalOnProperty(prefix="spring.datasource2.druid", name="username")
+    @ConfigurationProperties(prefix="spring.datasource2.druid")
+    public DataSource druidDataSource2() {
+    	return createDataSource(druidType);
+    }
+    
+    //----------------------------------------------配置事务管理器---------------------------------------------
  
     @Bean
     @Primary
-    @ConditionalOnBean(name="dataSource")
-    @ConditionalOnProperty(prefix="spring.datasource.datasource0", name="username")
+    @ConditionalOnMissingBean(name = "transactionManager")
     public DataSourceTransactionManager transactionManager(@Qualifier("dataSource") DataSource dataSource) {
     	return new DataSourceTransactionManager(dataSource);
     }
     
     @Bean
-    @ConditionalOnBean(name="datasource1")
-    public DataSourceTransactionManager transactionManager1(@Qualifier("datasource1") DataSource datasource1) {
-    	return new DataSourceTransactionManager(datasource1);
+    @ConditionalOnBean(name="dataSource1")
+    public DataSourceTransactionManager transactionManager1(@Qualifier("dataSource1") DataSource dataSource1) {
+    	return new DataSourceTransactionManager(dataSource1);
     }
 
     @Bean
-    @ConditionalOnBean(name="datasource2")
-    public DataSourceTransactionManager transactionManager2(@Qualifier("datasource2") DataSource datasource2) {
-    	return new DataSourceTransactionManager(datasource2);
+    @ConditionalOnBean(name="dataSource2")
+    public DataSourceTransactionManager transactionManager2(@Qualifier("dataSource2") DataSource dataSource2) {
+    	return new DataSourceTransactionManager(dataSource2);
     }
 
 }
