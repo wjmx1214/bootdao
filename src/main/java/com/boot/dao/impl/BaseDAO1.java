b61b2bf7a7d4d6a1e28a46b3b7dbff4914b643b7
wang.jia.le <wjmx1214@sina.com>
2021-04-07 20:22:54 +0800

    Update BaseDAO1.java

--- src/main/java/com/boot/dao/impl/BaseDAO1.java
+++ src/main/java/com/boot/dao/impl/BaseDAO1.java
@@ -1,21 +1,21 @@
 package com.boot.dao.impl;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.stereotype.Repository;
 
 import com.boot.dao.api.IBaseDAO1;
 
 /**
- * 数据源datasource1对应的DAO
+ * 数据源dataSource1对应的DAO
  * @author 2020-12-01 create wang.jia.le
- * @version 1.0.1
+ * @version 1.0.4
  */
 @Repository
-@ConditionalOnProperty(prefix="spring.datasource.datasource1", name="username") //配置了才注册
+@ConditionalOnBean(name = "dataSource1") //配置了才注册
 public class BaseDAO1 extends BaseEntityDAO implements IBaseDAO1{
 	
 	public BaseDAO1() {
-		super("datasource1");
+		super("dataSource1", "transactionManager1");
 	}
 
 }
