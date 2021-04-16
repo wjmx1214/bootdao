4bc13f1664c5e7ef8d5fd66fb0d20af356dcd1bb
wang.jia.le <wjmx1214@sina.com>
2021-04-16 15:34:47 +0800

    Update BaseTableMapping.java

--- src/main/java/com/boot/dao/mapping/BaseTableMapping.java
+++ src/main/java/com/boot/dao/mapping/BaseTableMapping.java
@@ -10,7 +10,7 @@ import com.boot.dao.util.BaseDAOLog;
 /**
  * 表映射
  * @author 2020-12-01 create wang.jia.le
- * @version 1.0.1
+ * @version 1.0.5
  */
 public class BaseTableMapping {
 
@@ -22,7 +22,7 @@ public class BaseTableMapping {
 	public boolean hasCreateTime = false;		//是否有创建时间, 用于DTO、VO类型新增判断(新增记录时根据配置决定是否自动生成)
 	public boolean isHump = true;				//是否开启驼峰转换
 	public boolean isEntity = true;				//当前映射是否为Entity
-	public int metaType = 0;					//当前实际的注解方式(没有或未知ID注解=0, EntityMeta=1, mybatis-plus=2, JPA=3)(=0时仅支持SQL查询)
+	public int mappingType = 0;					//当前实际的注解方式(没有或未知ID注解=0, EntityTable=1, mybatis-plus=2, JPA=3)(=0时仅支持SQL查询)
 
 	public Map<String, BaseColumnMapping> columnMappings = new HashMap<>(); //列映射集合(列名称:列映射)
 	public Map<String, BaseColumnMapping> fieldMappings = new HashMap<>(); 	//列映射集合(属性名:列映射)
