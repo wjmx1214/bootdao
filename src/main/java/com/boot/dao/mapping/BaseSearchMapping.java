f681f16502ac1b99b459716622e7c88d22f4a525
wang.jia.le <wjmx1214@sina.com>
2021-08-05 11:44:34 +0800

    update version to 1.0.8

--- src/main/java/com/boot/dao/mapping/BaseSearchMapping.java
+++ src/main/java/com/boot/dao/mapping/BaseSearchMapping.java
@@ -3,12 +3,14 @@ package com.boot.dao.mapping;
 import java.lang.reflect.Field;
 
 import com.boot.dao.api.SearchType;
+import com.boot.dao.api.Sort;
+import com.boot.dao.config.BaseDAOConfig;
 import com.boot.dao.util.BaseDAOLog;
 
 /**
  * 多条件动态查询映射
  * @author 2020-12-01 create wang.jia.le
- * @version 1.0.5
+ * @version 1.0.8
  */
 public class BaseSearchMapping {
 
@@ -16,11 +18,18 @@ public class BaseSearchMapping {
 	public String tableAs;			//表别名(默认="")
 	public String column;			//列名或列别名(默认=Field名称)(除非检测到驼峰转换显式关闭, 否则自动进行驼峰转下划线)
 	public int index;				//条件索引(多表或子查询时, 若出现多处where, 则利用此索引进行区分)(默认=1, 即默认只有一处where)
+	public Sort sort;				//排序规则
+	public String whereSQL;			//自定义条件语句，用于复杂的条件判断
 	Field searchField;				//对应的Field
+	boolean isDate;					//是否为日期类型
+	String formatTime = BaseDAOConfig.formatTime;	//当字段为日期类型时的格式化
 
 	public Object searchFieldGet(Object search){
 		try {
-			return searchField.get(search);
+			Object value = searchField.get(search);
+			if(isDate && value != null) {
+				return new java.text.SimpleDateFormat(formatTime).format(value);
+			}
 		} catch (Exception e) {
 			BaseDAOLog.printException(e);
 		}
