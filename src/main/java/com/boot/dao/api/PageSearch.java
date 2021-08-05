86f2ccfeb1d4963ffd6fa8ff7753b30e8c7e67d3
wang.jia.le <wjmx1214@sina.com>
2021-08-05 11:25:55 +0800

    update version to 1.0.7

--- src/main/java/com/boot/dao/api/PageSearch.java
+++ src/main/java/com/boot/dao/api/PageSearch.java
@@ -6,7 +6,7 @@ import java.util.List;
 /**
  * 多条件动态查询父类(分页)
  * @author 2020-12-01 create wang.jia.le
- * @version 1.0.1
+ * @version 1.0.7
  */
 public abstract class PageSearch extends BaseSearch{
 
@@ -36,13 +36,18 @@ public abstract class PageSearch extends BaseSearch{
 	
 	@Override
 	public final String appendWhere() {
+		return this.appendWhere(Integer.class); //由于函数多态问题，此处无法用null作为参数，后续使用时进行判断
+	}
+	
+	@Override
+	public final String appendWhere(Class<?> clz) {
 		if(super.append)
 			return super.SQL;
 		List<Object> params = new ArrayList<>();
 		List<String> qualifiers = super.findQualifier(SQL);
 		for (int i = 0; i < qualifiers.size(); i++) {
 			String qualifier = qualifiers.get(i);
-			String where = super.appendWhere(i+1, params);
+			String where = super.appendWhere(i+1, params, clz);
 			SQL = SQL.replace(qualifier, where);
 			if(countSQL != null) {
 				countSQL = countSQL.replace(qualifier, where);
@@ -60,11 +65,21 @@ public abstract class PageSearch extends BaseSearch{
 	 * @return String
 	 */
 	public final String appendWhere(String sql, String countSQL) {
+		return this.appendWhere(Integer.class); //由于函数多态问题，此处无法用null作为参数，后续使用时进行判断
+	}
+	
+	/**
+	 * @param sql
+	 * @param countSQL 总记录SQL, 可以不用编写<br>
+	 * 但考虑到复杂SQL的性能问题, 此处保留自定义功能, 但查询条件须与SQL相同
+	 * @return String
+	 */
+	public final String appendWhere(String sql, String countSQL, Class<?> clz) {
 		if(super.append)
 			return super.SQL;
 		super.SQL = sql;
 		this.countSQL = countSQL;
-		return this.appendWhere();
+		return this.appendWhere(clz);
 	}
 
 }
