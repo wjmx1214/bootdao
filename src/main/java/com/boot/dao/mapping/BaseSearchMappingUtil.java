e41c072497f6d5472abe3163275d7472d2735311
wang.jia.le <wjmx1214@sina.com>
2021-08-05 11:44:56 +0800

    update version to 1.0.8

--- src/main/java/com/boot/dao/mapping/BaseSearchMappingUtil.java
+++ src/main/java/com/boot/dao/mapping/BaseSearchMappingUtil.java
@@ -5,14 +5,18 @@ import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
 import com.boot.dao.api.Search;
 import com.boot.dao.api.SearchType;
+import com.boot.dao.api.Sort;
 import com.boot.dao.util.BaseDAOUtil;
 
 /**
  * 多条件动态查询映射工具类
  * @author 2020-12-01 create wang.jia.le
- * @version 1.0.5
+ * @version 1.0.8
  */
 abstract class BaseSearchMappingUtil {
 
@@ -47,11 +51,32 @@ abstract class BaseSearchMappingUtil {
 			if(sm.tableAs.length() > 0) {
 				sm.tableAs += ".";
 			}
+			sm.sort = search.sort();
+			sm.whereSQL = search.whereSQL();
 		}else {
 			sm.searchType = SearchType.eq;
 			sm.column = BaseDAOUtil.humpToUnderline(field.getName());
 			sm.tableAs = "";
 			sm.index = 1;
+			sm.sort = Sort.NOT;
+			sm.whereSQL = "";
+		}
+		//日期格式化
+		if(field.isAnnotationPresent(DateTimeFormat.class) && field.getType().getName().equals("java.util.Date")) {
+			DateTimeFormat format = field.getAnnotation(DateTimeFormat.class);
+			if(format.pattern().trim().length() > 0) {
+				sm.formatTime = format.pattern();
+				sm.isDate = true;
+			}else if(format.iso() != ISO.NONE) {
+				if(format.iso() != ISO.DATE) {
+					sm.formatTime = "yyyy-MM-dd";
+				}else if(format.iso() != ISO.TIME) {
+					sm.formatTime = "HH:mm:ss.SSSXXX";
+				}else if(format.iso() != ISO.DATE_TIME) {
+					sm.formatTime = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX";
+				}
+				sm.isDate = true;
+			}
 		}
 	}
 
