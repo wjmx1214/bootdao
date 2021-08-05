7c8a8f52db71cedc18a6920f8cc7b6ebf9dc2570
wang.jia.le <wjmx1214@sina.com>
2021-08-05 11:43:59 +0800

    update version to 1.0.7

--- src/main/java/com/boot/dao/mapping/BaseColumnMapping.java
+++ src/main/java/com/boot/dao/mapping/BaseColumnMapping.java
@@ -5,23 +5,29 @@ import java.lang.reflect.Field;
 /**
  * 列映射
  * @author 2020-12-01 create wang.jia.le
- * @version 1.0.1
+ * @version 1.0.7
  */
 public class BaseColumnMapping {
 
 	public String columnName = "";
+	public boolean saveMapping = true;
+	public boolean createMapping = true;
+	public boolean updateMapping = true;
 	public Field field;
-	public String formatDate;
+	public String formatTime;
 
 	public BaseColumnMapping(){}
 
-	public BaseColumnMapping(String columnName, Field field) {
+	public BaseColumnMapping(String columnName, Field field, boolean saveMapping, boolean createMapping, boolean updateMapping) {
 		this.columnName = columnName;
 		this.field = field;
+		this.saveMapping = saveMapping;
+		this.createMapping = createMapping;
+		this.updateMapping = updateMapping;
 	}
 
-	public BaseColumnMapping(String columnName, Field field, String formatDate) {
-		this(columnName, field);
-		this.formatDate = formatDate;
+	public BaseColumnMapping(String columnName, Field field, boolean saveMapping, boolean createMapping, boolean updateMapping, String formatTime) {
+		this(columnName, field, saveMapping, createMapping, updateMapping);
+		this.formatTime = formatTime;
 	}
 }
