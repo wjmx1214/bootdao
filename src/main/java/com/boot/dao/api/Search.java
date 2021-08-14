00164119f60ca3caa0ba4a308dfc93ea205e1eec
wang.jia.le <wjmx1214@sina.com>
2021-08-14 17:29:59 +0800

    update version to 1.1.0

--- src/main/java/com/boot/dao/api/Search.java
+++ src/main/java/com/boot/dao/api/Search.java
@@ -10,7 +10,6 @@ import org.springframework.core.annotation.AliasFor;
 
 /**
 	多条件动态查询注解, 作用于search类字段上, 用于自动封装查询条件, 简化开发<br>
-	注意：若该Search类用于多表共用时，调用appendWhere函数时，请带上实体类Class过滤非当前表的列，否则会出现SQL错误
 	例：<pre>
 	public class StuSearch extends PageSearch{ //BaseSearch
 
@@ -36,22 +35,22 @@ import org.springframework.core.annotation.AliasFor;
 
 	调用示例：
 	public Page<StuDto> pageStu(StuSearch search){
-		search.SQL = "(select * from stu where 1=1 #{where1或任意标识}) union (select * from stu s where s.on_class=1 #{where2或任意标识})";
+		search.SQL = "(select * from stu where 1=1 #{search1或任意标识}) union (select * from stu s where s.on_class=1 #{search2或任意标识})";
 		return baseDAO.page(search, StuDto.class);
 
 		or
 
-		//search.appendWhere("select * from stu where 1=1 #{where}"); //单表分页查询全量字段可省略SQL
+		//search.appendWhere("select * from stu where 1=1 #{search}"); //单表分页查询全量字段可省略SQL
 		return baseDAO.page(search, StuDto.class);
 	}
 
 	public List<StuDto> listStu(StuSearch search){
-		search.appendWhere("select * from stu where 1=1 #{where}");
+		search.appendWhere("select * from stu where 1=1 #{search}");
 		return baseDAO.getEntitys(search.SQL, StuDto.class, search.params);
 
 		or
 
-		search.SQL = "select * from stu where 1=1 #{where}";
+		search.SQL = "select * from stu where 1=1 #{search}";
 		return baseDAO.getEntitys(search.appendWhere(), StuDto.class, search.params);
 	}
 	</pre>
@@ -90,36 +89,46 @@ public @interface Search {
 	String column() default "";
 	
 	/**
-	 * 是否开启驼峰转换, 可省去配置列名(true=开启)
+	 * 是否开启驼峰转换<br>
+	 * 可省去配置列名(true=开启)
 	 * @return boolean
 	 */
 	boolean isHump() default true;
 
 	/**
-	 * 条件索引(多表或子查询时, 若出现多处where或having, 则利用此索引进行区分, 按阅读顺序)<br>
+	 * 条件索引<br>
+	 * 多表或子查询时, 若出现多处where或having, 则利用此索引进行区分, 按阅读顺序<br>
 	 * 默认=1, 即默认只有一处where
 	 * @return int
 	 */
 	int index() default 1;
 	
 	/**
-	 * 是否排序, 声明在前的属性优先加入排序规则<br>
-	 * 且不影响该属性作为查询条件(默认="")
+	 * 是否排序<br>
+	 * 声明在前的属性优先加入排序规则<br>
+	 * 且不影响该属性作为查询条件(默认="")<br>
+	 * 只要配置了Sort.DESC 或 Sort.ASC，不管字段是否为 null 都会加入到SQL排序中
 	 * @return Sort
 	 */
 	Sort sort() default Sort.NOT;
 	
 	/**
-	 * 自定义条件语句，用于复杂的条件判断<br>
-	 * 当该值不为空时，则直接并且只拼接该属性字段值<br>
+	 * 自定义条件语句<br>
+	 * 用于 or 或复杂的条件判断<br>
+	 * 例：whereSQL = "and (xx = ? or xxx = ? or xxxx = ?)"<br>
+	 * 例：whereSQL = "and (xx like %? or xxx like %?% or xxxx = ?)"<br>
+	 * 注意：当驼峰字段名转为下划线后，找不到对应的列不会进行拼接，须设置一个表中存在的‘任意’列名<br>
+	 * 当该值不为空时，则直接并且只拼接该注解属性值<br>
 	 * 并根据?号个数添加对应的参数，当为字符串类型时，带逗号的参数会进行分割对应<br>
-	 * 非字符串类型，或逗号分隔对应不上时则作为单值进行重复对应
+	 * 集合数组类型会以?号个数做匹配，多余的舍弃，不足则会抛出索引异常<br>
+	 * 非字符串类型，或字符串没有逗号分隔时，则作为单值进行重复对应<br>
 	 * @return String
 	 */
 	String whereSQL() default "";
 	
 	/**
-	 * 日期字段的格式，支持Date、Date[]、String、String(xx,xx)、String[]、List、Set类型<br>
+	 * 日期字段的格式<br>
+	 * 支持Date、Date[]、String、String(xx,xx)、String[]、List、Set类型<br>
 	 * 未配置时，则判断是否使用了@DateTimeFormat注解，都配置时，则以该属性优先<br>
 	 * 仅在拼接SQL时获取格式化后的日期，原值保持接收后的状态
 	 * @return String
@@ -132,5 +141,12 @@ public @interface Search {
 	 * @return boolean
 	 */
 	boolean isMapping() default true;
+	
+	/**
+	 * 业务类型<br>
+	 * 用于多个业务共用同一个Search时，区分字段属于哪个业务
+	 * @return String
+	 */
+	String businessName() default "";
 
 }
