8bb5961a532f1df995790af74075d44d5eee122d
wang.jia.le <wjmx1214@sina.com>
2021-08-18 04:51:39 +0800

    update version to 1.1.1

--- src/main/java/com/boot/dao/api/IBaseReadme.java
+++ src/main/java/com/boot/dao/api/IBaseReadme.java
@@ -113,7 +113,7 @@ public interface IBaseReadme {
 		private Long id;
 		+@Search(column="stu_name", type=SearchType.like_right)
 		private String name;
-		+@Search(column="stu_name", type=SearchType.like_all, tableAs="s", index=2)
+		+@Search(column="stu_name", type=SearchType.like_all, tableAs="s", whereKey="search2")
 		private String name2;
 	}
 
