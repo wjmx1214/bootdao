40d82818dd7235cfbd56a5b86c2c4d17ca77ff2d
wang.jia.le <wjmx1214@sina.com>
2025-01-22 00:55:11 +0800

    update version to 1.2.0

--- src/main/java/com/boot/dao/impl/BaseSource.java
+++ src/main/java/com/boot/dao/impl/BaseSource.java
@@ -17,7 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 /**
  * 数据源类
  * @author 2020-12-01 create wang.jia.le
- * @version 1.1.0
+ * @version 1.2.0
  */
 @Slf4j
 //装配基础配置类,spring上下文工具类,多数据源配置类,多数据源事务整合配置类
@@ -25,36 +25,15 @@ import lombok.extern.slf4j.Slf4j;
 abstract class BaseSource{
 
 	private String dataSourceName;
-
+	
 	private DataSource dataSource;
 	
 	protected BaseSourceType sourceType = BaseSourceType.mysql;
 	
-	@SuppressWarnings("unused")
-	private String transactionManagerName;
-	
-	public BaseSource(String dataSourceName, String transactionManagerName) {
-		this.dataSourceName = dataSourceName;
-		this.transactionManagerName = transactionManagerName;
-		String info =  "已启用数据源:"+dataSourceName + "#{}; 对应事务管理器:" + transactionManagerName;
-		if("dataSource".equals(dataSourceName)) {
-			info = info.replace("#{}", "(默认)\t调用请注入对应接口:com.boot.dao.api.I" + this.getClass().getSimpleName());
-		}else if("dataSource1".equals(dataSourceName) || "dataSource2".equals(dataSourceName)) {
-			info = info.replace("#{}", "\t\t调用请注入对应接口:com.boot.dao.api.I" + this.getClass().getSimpleName());
-		}else {
-			info = info.replace("#{}", "\t\t调用请注入对应DAO(或接口):" + this.getClass().getName());
-		}
-		log.info(info);
-	}
-
-	/**
-	 * 获取当前DAO所使用的数据源
-	 * @return DataSource
-	 * @throws SQLException
-	 */
-	protected DataSource getDataSource() throws SQLException{
-		if(dataSource == null) {
-			dataSource = ApplicationContextUtil.getBean(dataSourceName);
+	public void setDataSource(String dataSourceName, DataSource dataSource) throws SQLException {
+		if(this.dataSource == null) {
+			this.dataSourceName = dataSourceName;
+			this.dataSource = dataSource;
 			Connection conn = org.springframework.jdbc.datasource.DataSourceUtils.getConnection(dataSource);
 			String url = conn.getMetaData().getURL();
 			if(url.indexOf(":mysql:") > -1) {
@@ -68,6 +47,14 @@ abstract class BaseSource{
 			}
 			org.springframework.jdbc.datasource.DataSourceUtils.releaseConnection(conn, dataSource);
 		}
+	}
+	
+	/**
+	 * 获取当前DAO所使用的数据源
+	 * @return DataSource
+	 * @throws SQLException
+	 */
+	protected DataSource getDataSource() {
 		return dataSource;
 	}
 	
@@ -77,7 +64,7 @@ abstract class BaseSource{
 	 * @throws SQLException
 	 */
 	protected Connection getConnection() throws SQLException {
-		Connection conn = org.springframework.jdbc.datasource.DataSourceUtils.getConnection(this.getDataSource());
+		Connection conn = org.springframework.jdbc.datasource.DataSourceUtils.getConnection(this.dataSource);
 		if(BaseDAOConfig.showSource) {
 			log.info(new StringBuffer("\n\n----- 当前DAO类型: ").append(this.getClass().getName())
 					.append("; 数据源(").append(sourceType.toString()).append("): ").append(dataSource.getClass().getName() + "@" + Integer.toHexString(dataSource.hashCode()))
