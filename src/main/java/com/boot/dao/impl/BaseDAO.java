759612b733bd8b7d185380b499c4b0ab1a985529
wang.jia.le <wjmx1214@sina.com>
2025-01-22 00:53:22 +0800

    update version to 1.2.0

--- src/main/java/com/boot/dao/impl/BaseDAO.java
+++ src/main/java/com/boot/dao/impl/BaseDAO.java
@@ -1,23 +1,18 @@
 package com.boot.dao.impl;
 
-import org.springframework.stereotype.Repository;
-
 import com.boot.dao.api.IBaseDAO;
 
 /**
- * 数据访问封装类, 函数式调用, 适合无需DAO层的简单模型(封装Entity、DTO、VO无差别调用)<br>
- * 方法的Class参数可支持Entity.class, Dto.class, Vo.class 等符合实体模型的类型<br>
- * 甚至无需任何注解标识的类型(仅支持SQL查询)<br>
- * 可支持继承此类并加入@Repository注解, 继续封装独有需求的获取数据函数<br>
- * 除非有新增函数或重写函数需求, 否则不建议继承, 直接注入使用
+ * 数据访问封装接口, 泛型函数式API, 使用时无需定义DAO层<br>
+ * 注入方式示例:<br>
+ * ;@Autowired // mysql数据源<br>
+ * private IBaseDAO mysql; // 主数据源可省略注入名称<br>
+ * 或<br>
+ * ;@Autowired // clickhouse数据源<br>
+ * ;@Qualifier("clickhouse") <br>
+ * private IBaseDAO clickhouse;<br>
+ * 
  * @author 2020-12-01 create wang.jia.le
- * @version 1.0.4
+ * @version 1.2.0
  */
-@Repository //默认DAO, 已配置了自动注册
-public class BaseDAO extends BaseEntityDAO implements IBaseDAO{
-
-	public BaseDAO() {
-		super("dataSource", "transactionManager");
-	}
-	
-}
+public class BaseDAO extends BaseEntityDAO implements IBaseDAO{}
