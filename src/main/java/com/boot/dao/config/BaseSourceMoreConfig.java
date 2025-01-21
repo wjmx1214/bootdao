5469d73c25c19be3cd218fbc3274cef5d61df918
wang.jia.le <wjmx1214@sina.com>
2025-01-22 00:52:08 +0800

    update version to 1.2.0

--- src/main/java/com/boot/dao/config/BaseSourceMoreConfig.java
+++ src/main/java/com/boot/dao/config/BaseSourceMoreConfig.java
@@ -1,129 +1,178 @@
 package com.boot.dao.config;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.TransactionManager;
 
-import com.boot.dao.util.BaseDAOLog;
+import com.boot.dao.api.IBaseDAO;
+import com.boot.dao.impl.BaseDAO;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 多数据源默认配置类
  * @author 2020-12-01 create wang.jia.le
- * @version 1.0.6
+ * @version 1.2.0
  */
-@Configuration
+@Slf4j
+@Component
+@SuppressWarnings("unchecked")
 public class BaseSourceMoreConfig {
 
-	private static String druidType = "com.alibaba.druid.pool.DruidDataSource";
-	private static Method createMethod;
-	private static Method buildMethod;
+	private ApplicationContext applicationContext;
 
-	static {
-		try {
-			Class<?> druidDataSourceBuilderClass = Thread.currentThread().getContextClassLoader().loadClass("com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder");
-			createMethod = druidDataSourceBuilderClass.getDeclaredMethod("create");
-			buildMethod = druidDataSourceBuilderClass.getDeclaredMethod("build");
-		} catch (Exception e) {}
+	private ConfigurableEnvironment environment;
+
+	@Autowired
+	public void setApplicationContext(ApplicationContext applicationContext) {
+		log.info("开始初始化bootdao数据源...");
+		this.applicationContext = applicationContext;
+		this.environment = (ConfigurableEnvironment) applicationContext.getEnvironment();
+		init();
+		log.info("初始化bootdao数据源完成!!!");
 	}
-	
-    private DataSource createDataSource(String type) {
-    	DataSource dataSource = null;
-    	if(type != null && druidType.equals(type)) {
-    		if(buildMethod != null) {
-				try {dataSource = (DataSource)buildMethod.invoke(createMethod.invoke(null)); //阿里巴巴连接池druid创建方式
-				} catch (Exception e) {BaseDAOLog.printException(e);} 
-			}else {
-				BaseDAOLog.printException(new ClassNotFoundException("not found " + druidType));
+
+	private void init() {
+		// 获取注册器
+		BeanDefinitionRegistry registry = (BeanDefinitionRegistry) applicationContext.getAutowireCapableBeanFactory();
+		
+		// 获取spring默认数据源
+		Map<String, Map<DataSource, String>> dms = new HashMap<>();
+		DataSource defaultDataSource = null;
+		try {
+			defaultDataSource = applicationContext.getBean(DataSource.class);
+		}catch (Exception e) {
+			log.error("默认dataSource数据源初始化失败，请检查配置!!!", e);
+			System.exit(0);
+		}
+
+		// 创建其它方式的数据源并注册到spring
+		Map<String, DataSource> dataSources = DataSourceMoreCreate.createOtherDataSource(environment);
+
+		// 获取baomidou方式的数据源
+		String primaryDataSourceName = null;
+		List<Object> baomidouDataSources = getBaomidouDataSource(defaultDataSource);
+		if (baomidouDataSources != null) {
+			primaryDataSourceName = (String) baomidouDataSources.get(0);
+			dataSources.putAll((Map<String, DataSource>) baomidouDataSources.get(1));
+		} else {
+			primaryDataSourceName = "dataSource";
+			dataSources.put(primaryDataSourceName, defaultDataSource);
+			Map<DataSource, String> defaultDm = new HashMap<>();
+			defaultDm.put(defaultDataSource, "transactionManager");
+			dms.put(primaryDataSourceName, defaultDm);
+		}
+		
+		// 注册数据源和事务管理器
+		dataSources.forEach((dataSourceName, dataSource) -> {
+			if(!"dataSource".equals(dataSourceName)) {
+				registerDataSourceBean("_" + dataSourceName, dataSource, registry); // 注册数据源
+				TransactionManager transactionManager = new DataSourceTransactionManager(dataSource);
+				String transactionManagerName = "transactionManager_" + dataSourceName;
+				registerTransactionManager(transactionManagerName, transactionManager, registry); // 注册事务管理器
+				Map<DataSource, String> dm = new HashMap<>();
+				dm.put(dataSource, transactionManagerName);
+				dms.put(dataSourceName, dm);
 			}
-    		//dataSource = com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder.create().build(); //阿里巴巴连接池druid创建方式
-    	}else {
-    		dataSource = DataSourceBuilder.create().build(); //Spring默认使用的连接池创建方式(未配置type时为hikariCP)
-    	}
-    	return dataSource;
-    }
+		});
 
-    @Bean
-    @Primary
-    @ConditionalOnMissingBean(name = "dataSource")
-    @ConditionalOnProperty(prefix="spring.datasource", name="username")
-    @ConfigurationProperties(prefix="spring.datasource")
-    @Value("${spring.datasource.type:#{null}}")
-    public DataSource dataSource(String type) {
-    	return createDataSource(type);
-    }
-	
-    @Bean
-    @ConditionalOnProperty(prefix="spring.datasource1", name="username")
-    @ConfigurationProperties(prefix="spring.datasource1")
-    @Value("${spring.datasource1.type:#{null}}")
-    public DataSource dataSource1(String type) {
-    	return createDataSource(type);
-    }
+		// 统一注册bootdao数据源
+		registryBootdao(registry, dms, primaryDataSourceName);
+	}
 
-    @Bean
-    @ConditionalOnProperty(prefix="spring.datasource2", name="username")
-    @ConfigurationProperties(prefix="spring.datasource2")
-    @Value("${spring.datasource2.type:#{null}}")
-    public DataSource dataSource2(String type) {
-    	return createDataSource(type);
-    }
-    
-    //----------------------------------------------druid-----------------------------------------------------
-    
-    @Bean(name = "dataSource")
-    @Primary
-    @ConditionalOnMissingBean(name = "dataSource")
-    @ConditionalOnProperty(prefix="spring.datasource.druid", name="username")
-    @ConfigurationProperties(prefix="spring.datasource.druid")
-    public DataSource druidDataSource() {
-    	return createDataSource(druidType);
-    }
-	
-    @Bean(name = "dataSource1")
-    @ConditionalOnProperty(prefix="spring.datasource1.druid", name="username")
-    @ConfigurationProperties(prefix="spring.datasource1.druid")
-    public DataSource druidDataSource1() {
-    	return createDataSource(druidType);
-    }
+	// 统一注册bootdao数据源
+	public void registryBootdao(BeanDefinitionRegistry registry, Map<String, Map<DataSource, String>> dms,
+			String primaryDataSourceName) {
+		for (String dataSourceName : dms.keySet()) {
+			Map<DataSource, String> dm = dms.get(dataSourceName);
+			dm.forEach((dataSource, transactionManagerName) -> {
+				// 创建一个 GenericBeanDefinition 以定义 BaseDAO 的 Bean
+				GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
+				beanDefinition.setBeanClass(IBaseDAO.class); // 设置 Bean 类
 
-    @Bean(name = "dataSource2")
-    @ConditionalOnProperty(prefix="spring.datasource2.druid", name="username")
-    @ConfigurationProperties(prefix="spring.datasource2.druid")
-    public DataSource druidDataSource2() {
-    	return createDataSource(druidType);
-    }
-    
-    //----------------------------------------------配置事务管理器---------------------------------------------
- 
-    @Bean
-    @Primary
-    @ConditionalOnMissingBean(name = "transactionManager")
-    public DataSourceTransactionManager transactionManager(@Qualifier("dataSource") DataSource dataSource) {
-    	return new DataSourceTransactionManager(dataSource);
-    }
-    
-    @Bean
-    @ConditionalOnBean(name="dataSource1")
-    public DataSourceTransactionManager transactionManager1(@Qualifier("dataSource1") DataSource dataSource1) {
-    	return new DataSourceTransactionManager(dataSource1);
-    }
+				// 定义 Bean 的实例化逻辑
+				beanDefinition.setInstanceSupplier(() -> {
+					IBaseDAO baseDAO = new BaseDAO(); // 新实例化 BaseDAO 的实现类
+					try {
+						baseDAO.setDataSource("_"+dataSourceName, dataSource); // 设置 DataSource
+					} catch (SQLException e) {
+						log.error(e.getMessage(), e);
+					} 
+					return baseDAO; // 返回初始化好的 BaseDAO 实例
+				});
+				
+				if(!"dataSource".equals(dataSourceName)) {
+					registry.registerBeanDefinition(dataSourceName, beanDefinition);
+					log.info("已将数据源({})注册到IBaseDAO,注册名称为:{},事务管理器名称为:{}", "_"+dataSourceName, dataSourceName, transactionManagerName);
+				}
+				// 主数据源多注册一个别名
+				if (dataSourceName.equals(primaryDataSourceName)) {
+					registry.registerBeanDefinition("baseDAO", beanDefinition);
+					log.info("已将主数据源({})注册到IBaseDAO,注册名称为:{},事务管理器名称为:{}", "_"+primaryDataSourceName, "baseDAO", transactionManagerName);
+				}
+			});
+		}
+	}
 
-    @Bean
-    @ConditionalOnBean(name="dataSource2")
-    public DataSourceTransactionManager transactionManager2(@Qualifier("dataSource2") DataSource dataSource2) {
-    	return new DataSourceTransactionManager(dataSource2);
-    }
+	// 注册数据源 Bean 到 Spring 容器
+	private void registerDataSourceBean(String name, DataSource dataSource, BeanDefinitionRegistry registry) {
+		GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
+		beanDefinition.setBeanClass(dataSource.getClass());
+		beanDefinition.setInstanceSupplier(() -> dataSource);
+
+		// 注册数据源到 Spring 的 BeanDefinitionRegistry
+		registry.registerBeanDefinition(name, beanDefinition);
+	}
+
+	// 注册事务管理器方法
+	private void registerTransactionManager(String name, TransactionManager transactionManager,
+			BeanDefinitionRegistry registry) {
+		GenericBeanDefinition transactionManagerDefinition = new GenericBeanDefinition();
+		transactionManagerDefinition.setBeanClass(transactionManager.getClass());
+		transactionManagerDefinition.setInstanceSupplier(() -> transactionManager); // 直接返回实例
+
+		// 注册事务管理器到 Spring 的 BeanDefinitionRegistry
+		registry.registerBeanDefinition(name, transactionManagerDefinition);
+	}
+
+	// 获取baomidou方式的数据源
+	private List<Object> getBaomidouDataSource(DataSource defaultDataSource) {
+		if(defaultDataSource != null) {
+			try {
+				Class<?> dynamicRoutingDataSourceClass = Thread.currentThread().getContextClassLoader()
+						.loadClass("com.baomidou.dynamic.datasource.DynamicRoutingDataSource");
+				if (dynamicRoutingDataSourceClass != null) {
+					List<Object> list = new ArrayList<>();
+					Field primaryField = dynamicRoutingDataSourceClass.getDeclaredField("primary");
+					if(primaryField != null) {
+						primaryField.setAccessible(true); // 强制访问
+						String primary = (String) primaryField.get(defaultDataSource);
+						list.add(primary); // 主数据源名称
+		
+						Method getCurrentDataSources = dynamicRoutingDataSourceClass.getMethod("getCurrentDataSources");
+						Map<String, DataSource> dataSources = (Map<String, DataSource>) getCurrentDataSources.invoke(defaultDataSource);
+						list.add(dataSources); // 所有数据源
+						return list;
+					}
+				}
+			} catch (Exception e) {}
+		}
+		return null;
+	}
 
 }
